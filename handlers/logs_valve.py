1f099645f256efc716c7af66ebd5e55515f4d064
savabush <vatka1337@gmail.com>
2022-05-27 13:33:17 +0700

    Added handler logs of server's script LGSM

--- handlers/logs_valve.py
+++ handlers/logs_valve.py
@@ -4,6 +4,7 @@ from aiogram import types, Dispatcher
 # Api
 from valve_api.logs.logs_server import get_logs_server
 from valve_api.logs.logs_console import get_logs_console
+from valve_api.logs.logs_script import get_logs_script
 
 
 async def logs_server(msg: types.Message):
@@ -16,6 +17,12 @@ async def logs_console(msg: types.Message):
     await msg.answer(result)
 
 
+async def logs_script(msg: types.Message):
+    result = get_logs_script()
+    await msg.answer(result)
+
+
 def register_handler_logs_valve(dp: Dispatcher):
     dp.register_message_handler(logs_server, lambda msg: msg.text == 'Логи сервера')
     dp.register_message_handler(logs_console, lambda msg: msg.text == 'Логи консоли')
+    dp.register_message_handler(logs_script, lambda msg: msg.text == 'Логи скрипта')
---
+++ valve_api/logs/logs_script.py
@@ -0,0 +1,10 @@
+from ssh import ssh_connect
+
+
+def get_logs_script():
+    """
+    Get logs of server's script LGSM
+    :return:
+    """
+    result = ssh_connect('cd csgo/log/script/ && tail -n 50 csgoserver-script.log')
+    return result
